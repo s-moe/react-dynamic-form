5b3e8db3d5a3b7af1abb3572aa813b12eb05b3d0
Sarah Moe <sarahannmoe@gmail.com>
2021-12-02 13:34:23 -0800

    created handleSubmit

--- src/components/DynamicForm.js
+++ src/components/DynamicForm.js
@@ -1,7 +1,23 @@
 import { useState } from "react";
 
 export default function DynamicForm({ data }) {
+  //set state for user input
   const [userData, setUserData] = useState(data);
+  //need a handleSubmit for submit button
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUserData({});
+    console.log(userData);
+  };
 
-  return <div>Testing</div>;
+  //need a handleChange for user's input
+  //need the function for determining date
+
+  //render a dynamic form using the json data
+  //render checkbox conditionally (based on birthdate)
+  return (
+    <div>
+      <form onSubmit={handleSubmit}></form>
+    </div>
+  );
 }
